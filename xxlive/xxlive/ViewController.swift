c95d89a48c67feca122a1d05555551933a7b32af
wangxingdaren <wangxingdaren@foxmail.com>
2016-03-14 23:30:35 +0800

    创建项目

---
+++ xxlive/xxlive/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  xxlive
+//
+//  Created by 王杏 on 16/3/14.
+//  Copyright © 2016年 王杏. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
